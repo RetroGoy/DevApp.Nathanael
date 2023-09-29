3418a87e9eb7ef40a1505b2e77ed875369b10b48
Nathanaël NAVEAU <nnaveau@factorielles.fr>
2023-09-29 14:48:00 +0200

    config + sql

--- config.php
+++ config.php
@@ -10,4 +10,12 @@ try {
 } catch (PDOException $e) {
     echo "Erreur de connexion: " . $e->getMessage();
 }
+
+$sql = "CREATE TABLE IF NOT EXISTS contact (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    pseudo VARCHAR(50) NOT NULL,
+    pass VARCHAR(255) NOT NULL,  // Nous utiliserons un hash pour le mot de passe
+    dateinscription DATETIME NOT NULL
+)";
+$pdo->exec($sql);
 ?>
